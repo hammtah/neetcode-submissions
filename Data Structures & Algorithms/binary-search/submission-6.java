a6ff307e0c06ae080a999945fe01f8c9e99e67ce
Taha Hammadate <115116715+hammtah@users.noreply.github.com>
2026-09-14 10:02:24 +0100

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1, m = 0;
+        while(l <= r) {
+            m = (r - l) / 2 + l;
+            if (nums[m] == target) return m;
+            else if (nums[m] < target) l = m + 1;    
+            else r = m - 1;    
+        }
+        return -1;
+    }
+}
